5956b7e26c80140534b4c888606180608c576da6
tugcekrrgz <123090853+tugcekrrgz@users.noreply.github.com>
2023-09-28 21:51:01 +0300

    Exchange API

--- Exchange.API/Controllers/ExchangeController.cs
+++ Exchange.API/Controllers/ExchangeController.cs
@@ -1,5 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Exchange.API.DTOs;
 using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
 
 namespace Exchange.API.Controllers
 {
@@ -7,10 +8,54 @@ namespace Exchange.API.Controllers
     [ApiController]
     public class ExchangeController : ControllerBase
     {
+        private readonly HttpClient _client;
+
+        public ExchangeController(HttpClient client)
+        {
+            _client = client;
+        }
+
         [HttpGet]
-        public IActionResult GetCurrency()
+        public async Task<IActionResult> GetCurrency()
+        
         {
             string url = "https://www.tcmb.gov.tr/kurlar/today.xml";
+
+            List<ExchangeDTO> exchanges = new List<ExchangeDTO>();
+
+            var result=await _client.GetAsync(url);
+
+            if (result.IsSuccessStatusCode)
+            {
+                var xmlContent = await result.Content.ReadAsStringAsync();
+                XDocument xml = XDocument.Parse(xmlContent);
+                var currencies = xml.Descendants("Currency");
+
+                foreach (var currency in currencies)
+                {
+                    ExchangeDTO exchangeDTO = new ExchangeDTO();
+
+                    exchangeDTO.CurrencyCode=currency.Element("CurrencyName")?.Value;
+                    exchangeDTO.CurrencyName = currency.Element("Isim")?.Value;
+
+                    if (!string.IsNullOrEmpty(currency.Element("ForexBuying")?.Value)){
+                        exchangeDTO.ForexBuying = decimal.Parse(currency.Element("ForexBuying")?.Value.Replace('.', ','));
+                    }
+                    if (!string.IsNullOrEmpty(currency.Element("ForexSelling")?.Value))
+                    {
+                        exchangeDTO.ForexSelling = decimal.Parse(currency.Element("ForexSelling")?.Value.Replace('.', ','));
+                    }                  
+
+
+                    exchanges.Add(exchangeDTO);                 
+                }
+                return Ok(exchanges);
+            }
+            else
+            {
+                return BadRequest();
+            }
+
         }
     }
 }
--- Exchange.API/Program.cs
+++ Exchange.API/Program.cs
@@ -4,6 +4,9 @@ builder.Services.AddControllers();
 
 // Add services to the container.
 
+//HTTP Client
+builder.Services.AddHttpClient();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
